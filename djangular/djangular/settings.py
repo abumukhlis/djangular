b62a3f7c0d156653c58a51126b6d456cee73e9b3
Azeez Lawal <azeelaw@gmail.com>
2020-03-20 16:47:19 +0100

    Modified django version to 1.9.1 from 3.0 and installed the django-rest-framework.

--- djangular/djangular/settings.py
+++ djangular/djangular/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scrumboard',
+    'rest_framework',
+    'scrumboard'
 ]
 
 MIDDLEWARE = [
