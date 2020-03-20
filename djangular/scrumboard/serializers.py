a506152187f2703db1fa98c98a8ddd5afdbe9a8a
Azeez Lawal <azeelaw@gmail.com>
2020-03-20 22:23:10 +0100

    Retrieving data using $http

--- djangular/scrumboard/serializers.py
+++ djangular/scrumboard/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 
 from .models import List, Card
 
-class ListSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = List
-
-
 class CardSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Card
+
+
+class ListSerializer(serializers.ModelSerializer):
+    cards = CardSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = List
